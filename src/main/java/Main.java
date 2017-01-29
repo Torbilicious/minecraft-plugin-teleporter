5043dd988840d1b320270116284f2e5d598a7b8a
Torben Binder <torben.binder@googlemail.com>
2017-01-29 21:54:01 +0100

    test commit for Ike!

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,11 @@
 /**
  * Created by Torben Binder on 29.01.2017.
  */
+
 public class Main {
+
+    public static void main(String... args) {
+
+        System.out.println("Hello World!");
+    }
 }
