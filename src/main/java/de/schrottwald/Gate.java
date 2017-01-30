ac5ff6015b4165dd2a284fec3c8dd23e57d798f1
Torben Binder <torben.binder@googlemail.com>
2017-01-30 20:34:20 +0100

    cleanup

--- src/main/java/de/schrottwald/Gate.java
+++ src/main/java/de/schrottwald/Gate.java
@@ -10,25 +10,9 @@ public class Gate {
     private final Location location;
     private final String name;
 
-    public Gate(Location location, String name) {
-
-        this.location = location;
-        this.name = name;
-    }
-
     public Gate(Player player) {
 
         this.location = player.getLocation();
         this.name = player.getName();
-    }
-
-    public Location getLocation() {
-
-        return location;
-    }
-
-    public String getName() {
-
-        return name;
     }
 }
