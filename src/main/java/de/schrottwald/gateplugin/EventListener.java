bf04fb8d135d757054b80a1e65bad6b465875ab4
Torben Binder <torben.binder@googlemail.com>
2017-02-01 22:29:57 +0100

    reworked method for better readability

--- src/main/java/de/schrottwald/gateplugin/EventListener.java
+++ src/main/java/de/schrottwald/gateplugin/EventListener.java
@@ -1,6 +1,8 @@
 package de.schrottwald.gateplugin;
 
-import org.bukkit.ChatColor;
+import de.schrottwald.gateplugin.gate.Gate;
+import de.schrottwald.gateplugin.gate.GateNetwork;
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.Sign;
@@ -12,9 +14,6 @@ import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import static org.bukkit.Bukkit.getLogger;
 
@@ -75,44 +74,39 @@ public class EventListener implements Listener {
 
         if (e.getAction() == Action.RIGHT_CLICK_BLOCK) {
 
+            //TODO: check if this sign has the correct gate syntax
+
             if (e.getClickedBlock().getState() instanceof Sign) {
-                List<String> gateList = new ArrayList<>();
+                GateNetwork network = new GateNetwork("");
+                Location tempLocation = new Location(plugin.getServer().getWorld("world"), 0, 0, 0);
+
                 for (int i = 1; i < 10; i++) {
-                    gateList.add("Gate" + i);
+                    network.addNewGate(tempLocation, "Gate" + i);
                 }
 
                 Sign s = (Sign) e.getClickedBlock().getState();
 
-                int index = 0;
-                for (int i = 0; i < gateList.size(); i++) {
-                    String line2 = s.getLine(2);
-                    String text = line2.substring(line2.indexOf(">§a") + 1, line2.indexOf("§0<"));
-//                    String text2 = ((line2).replaceAll(">§a","")).replaceAll("§0<","");
-                    if(gateList.get(i).equals(text)) {
-                        index = i;
-                    }
-                }
-                /* Kann ich für die umgekehrte Funktion noch gebrauchen
-                if (index - 1 < 0) {
-                    s.setLine(0,gateList.get(gateList.size()));
-                } else {
-                    s.setLine(0,gateList.get(index - 1));
-                }
-                */
+                Gate target = network.getGateByName(extractGateName(s.getLine(2)));
+
                 s.setLine(1,s.getLine(2));
-                s.setLine(2,">" + ChatColor.GREEN + s.getLine(3) + ChatColor.BLACK + "<");
-                if (index + 2 == gateList.size()) {
-                    s.setLine(3, gateList.get(0));
-                } else if (index + 2 > gateList.size()) {
-                    s.setLine(3, gateList.get(1));
-                } else {
-                    s.setLine(3,gateList.get(index + 2));
-                }
+                s.setLine(2,s.getLine(3));
+
+                Gate gateLine4 = network.getGateByName(s.getLine(3));
+
+                s.setLine(3, network.getNextGate(gateLine4).getName());
+
                 s.update();
-                getLogger().info("" + index + "/" + gateList.size());
-                getLogger().info("" + ((s.getLine(2)).replaceAll(">§a","")).replaceAll("§0<",""));
+                Integer index = network.getIndexOfGate(s.getLine(2));
+                getLogger().info("" + index + "/" + network.getNumberOfGates());
+                getLogger().info("Target: " + target.getName());
                 getLogger().info("" + s.getLine(2));
             }
         }
+    }
+
+    private String extractGateName(String line) {
+
+//        return line.substring(line.indexOf(">§a") + 1, line.indexOf("§0<"));
+        return line;
     }
 }
